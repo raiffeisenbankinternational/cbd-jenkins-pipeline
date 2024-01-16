d4d0460f09b435b06f237117bfbaa54cba1b05c8
ewp10022 <robert.pofuk@rbinternational.com>
2024-01-16 08:43:13 +0100

    Fix

--- scripts/repo.groovy
+++ scripts/repo.groovy
@@ -8,8 +8,8 @@ def upload(String file, String pomFile, String artifactId, String groupId, Strin
                ARTIFACT_NAME=\$(basename \${FILE})
                PACKAGING=\${ARTIFACT_NAME##*.}
                echo "Adding licence information: "
-               sed -i 's/<\\/project>/<license><name>Apache-2.0<\\/name><url>https:\\/\\/www.apache.org\\/licenses\\/LICENSE-2.0.txt<\\/url><\\/license><\\/project>/g' pom.xml
-               cat pom.xml
+               sed -i 's/<\\/project>/<license><name>Apache-2.0<\\/name><url>https:\\/\\/www.apache.org\\/licenses\\/LICENSE-2.0.txt<\\/url><\\/license><\\/project>/g' ${pomFile}
+               cat ${pomFile}
                echo "Deploying: \${GROUP_ID}:\${ARTIFACT_ID} on file \${FILE} packaged as \${PACKAGING}  with version: \${VERSION}"
                mvn deploy:deploy-file \
                  -DgroupId=\${GROUP_ID} \
